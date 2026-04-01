d4b7f647cb2a2bb7c0d6abcbb17cb8012aa12ecd
Rtamanyu N J <rtamanyu@gmail.com>
2026-04-01 10:10:35 +0530

    Modified the guthub job to add the new test code

--- test/valet_api_test.ts
+++ test/valet_api_test.ts
@@ -37,62 +37,6 @@ async function run() {
   await waitForHealth();
   const plate = `TEST-${Date.now()}`;
 
-  // // test adding a valet entry
-  //   const createResponse = await fetch(`${API_BASE}/update_valet_state`, {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'X-Restaurant-Id': RESTAURANT_ID,
-  //   },
-  //   body: JSON.stringify({ number_plate: plate, state: 0 }),
-  // });
-
-  // if (!createResponse.ok) {
-  //   const txt = await createResponse.text();
-  //   throw new Error(`POST /update_valet_state failed: ${createResponse.status} ${txt}`);
-  // }
-
-  // console.log('✅ Valet Creation passed');
-
-  // // Test get valet state
-  // const r = await fetch(`${API_BASE}/get_valet_state`, {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'X-Restaurant-Id': RESTAURANT_ID,
-  //   },
-  //   body: JSON.stringify({ number_plate: plate }),
-  // });
-
-  // if (!r.ok) {
-  //   const txt = await r.text();
-  //   throw new Error(`GET /get_valet_state failed: ${r.status} ${txt}`);
-  // }
-
-  // const body = await r.json();
-  // if (!body || typeof body !== 'object' || !('valet_state' in body)) {
-  //   console.error('Unexpected response body:', body);
-  //   throw new Error('Valet response did not contain expected fields');
-  // }
-  // console.log('✅ Valet get state passed');
-
-  // // Test update valet state
-  // const updateResponse = await fetch(`${API_BASE}/update_valet_state`, {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'X-Restaurant-Id': RESTAURANT_ID,
-  //   },
-  //   body: JSON.stringify({ number_plate: plate, state: 2 }),
-  // });
-
-  // if (!updateResponse.ok) {
-  //   const txt = await updateResponse.text();
-  //   throw new Error(`POST /update_valet_state failed: ${updateResponse.status} ${txt}`);
-  // }
-
-  // console.log('✅ Valet update state passed');
-
   interface CreateValetResponse {
     message: string;
     booking_id: string;
