8d540eef62e28ebf447f440c6dff70fcc8e87b6b
Rtamanyu N J <rtamanyu@gmail.com>
2026-04-26 16:50:33 +0530

    fixxed import error

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+// import express, { Request, Response } from 'express';
 import serverless from 'serverless-http';
 import { app as createApp } from '../../index.js'; // your main express app
 
